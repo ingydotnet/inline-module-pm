7d9cdf9d6125c7f0e557710aa2968565382df29b
Ingy döt Net <ingy@ingy.net>
2014-10-31 01:10:31 +0000

    Halfway to getting blib built right.

--- lib/Inline/Module.pm
+++ lib/Inline/Module.pm
@@ -57,23 +57,46 @@ sub create_module {
     print "Inline module '$module' created as '$filepath'\n";
 }
 
-sub importer {
-    require File::Path;
-    File::Path::mkpath('./blib') unless -d './blib';
-    # TODO try to not use eval here:
-    eval "use Inline Config => directory => './blib'";
-
-    splice(\@_, 0, 1, 'Inline');
-    goto &Inline::import;
-}
-
 sub import {
     my $class = shift;
     return unless @_;
     my ($inline_module) = caller;
-    $class->check_api_version($inline_module, @_) or exit 1;
+    # XXX exit here is to get cleaner error msg. Try to redo without exit.
+    $class->check_api_version($inline_module, @_)
+        or exit 1;
+    my $importer = sub {
+        require File::Path;
+        File::Path::mkpath('./blib') unless -d './blib';
+        # TODO try to not use eval here:
+        eval "use Inline Config => directory => './blib'";
+
+        my $class = shift;
+        Inline->import_heavy(@_);
+
+        my $file = $inline_module;
+        $file =~ s/.*:://;
+        my $name_path = $inline_module;
+        $name_path =~ s!::!/!g;
+        File::Path::mkpath("blib/lib/$name_path");
+        open OUT, '>', "blib/lib/$name_path.pm"
+            or die $!;
+        print OUT <<"...";
+use strict; use warnings;
+package $inline_module;
+use DynaLoader;
+our \@ISA = qw( DynaLoader );
+our \$VERSION = '0.0.5';
+bootstrap $inline_module \$VERSION;
+1;
+...
+        close OUT;
+        File::Path::mkpath("blib/arch/auto/$name_path");
+        link "blib/lib/auto/Acme/Math/XS_c2a9/XS_c2a9.so",
+            "blib/arch/auto/$name_path/$file.so";
+    };
     no strict 'refs';
-    *{"${inline_module}::import"} = \&importer;
+    *{"${inline_module}::import"} = $importer;
+
 }
 
 sub check_api_version {
@@ -106,6 +129,7 @@ sub proxy_module {
 use strict;
 use warnings;
 package $module;
+use base 'Inline';
 
 use Inline::Module 'v1' => '$VERSION';
 
